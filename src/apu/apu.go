57bf794d8c5a7e88174d275ee1585b6a95184cc0
djhworld <djharperuk@gmail.com>
2013-02-28 22:24:12 +0000

    Added placeholder for the soundcard just so it swallows up all the write warnings for the sound registers in the MMU

---
+++ src/apu/apu.go
@@ -0,0 +1,36 @@
+/**
+ * Just a place holder for now
+ * User: danielharper
+ * Date: 28/02/2013
+ * Time: 21:26
+ */
+package apu
+
+import (
+	"log"
+	"types"
+)
+
+type APU struct{}
+
+func NewAPU() *APU {
+	var a *APU = new(APU)
+	a.Reset()
+	return a
+}
+
+func (apu *APU) Name() string {
+	return "APU"
+}
+
+func (apu *APU) Read(Address types.Word) byte {
+	return 0x00
+}
+
+func (apu *APU) Write(Address types.Word, Value byte) {
+
+}
+
+func (apu *APU) Reset() {
+	log.Println("Resetting", apu.Name())
+}
