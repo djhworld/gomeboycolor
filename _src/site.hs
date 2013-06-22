148b47992fb8b19b7b05285a60af8cc9866fbb56
djhworld <djharperuk@gmail.com>
2013-06-22 22:04:37 +0100

    Updated static site

---
+++ _src/site.hs
@@ -0,0 +1,47 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Control.Applicative ((<$>))
+import           Data.Monoid         (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+config :: Configuration
+config = defaultConfiguration
+        {   deployCommand = "cp -a ./_site/* ../" }
+
+
+main :: IO ()
+main = hakyllWith config $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match (fromList ["index.markdown"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+
+    match "templates/*" $ compile templateCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
+
+--------------------------------------------------------------------------------
+postList :: ([Item String] -> [Item String]) -> Compiler String
+postList sortFilter = do
+    posts   <- sortFilter <$> loadAll "posts/*"
+    itemTpl <- loadBody "templates/post-item.html"
+    list    <- applyTemplateList itemTpl postCtx posts
+    return list
