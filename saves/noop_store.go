8c5293ef0fc46c631ba6fc176505febe73aa2794
djhworld <djharperuk@gmail.com>
2018-09-03 20:55:35 +0100

    reconfiguring gitignore

---
+++ saves/noop_store.go
@@ -0,0 +1,39 @@
+package saves
+
+import (
+	"io"
+	"log"
+)
+
+type NoopStore struct {
+	baseDir string
+}
+
+func NewNoopStore() *NoopStore {
+	return new(NoopStore)
+}
+
+func (n *NoopStore) Open(game string) (io.ReadCloser, error) {
+	return new(NoopContent), nil
+}
+
+func (n *NoopStore) Create(game string) (io.WriteCloser, error) {
+	return new(NoopContent), nil
+}
+
+type NoopContent struct{}
+
+func (c *NoopContent) Write(b []byte) (int, error) {
+	log.Println("Saving to a NOOP store")
+	log.Println(string(b))
+
+	return len(b), nil
+}
+
+func (c *NoopContent) Read(p []byte) (int, error) {
+	return len(p), nil
+}
+
+func (c *NoopContent) Close() error {
+	return nil
+}
